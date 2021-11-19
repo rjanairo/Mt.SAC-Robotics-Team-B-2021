ef5b912c5f6f6785364a45863ddde49569300440
Russel Janairo <61097770+rjanairo@users.noreply.github.com>
2021-11-18 18:02:28 -0800

    new arm back
    new arm back

--- Zika Code Build/#Old Alumni Code/Competition-Template-VEXcode-main (Jovany)/Competition-Template-VEXcode-main/VEX U Competition template/src/main.cpp
+++ Zika Code Build/#Old Alumni Code/Competition-Template-VEXcode-main (Jovany)/Competition-Template-VEXcode-main/VEX U Competition template/src/main.cpp
@@ -1,24 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       VEX                                                       */
-/*    Created:      Wed Sep 25 2019                                           */
-/*    Description:  Clawbot (Drivetrain) - Template                           */
-/*                                                                            */
-/*    Name:                                                                   */
-/*    Date                                                                    */
-/*    Class:                                                                  */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
-
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Drivetrain           drivetrain    1, 10, D
-// ClawMotor            motor         3
-// ArmMotor             motor         8
-// ---- END VEXCODE CONFIGURED DEVICES ----
-
 #include "vex.h"
 #include "driver-control.h"
 #include "routines.h"
@@ -28,18 +7,15 @@ using namespace vex;
 
 void Usercontrol(){
   while(1){
-    
     DriveArcade();
+    IntakeControl();
+    //LiftControl();
     //wait to save resources
     wait(10,msec);
   }
-
-
 }
 
-
 void Autonomous(){
-
   RoutineOne();
 }
 
@@ -48,8 +24,7 @@ competition Competition;
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-
-  //Use Competition Functions
+ 
   Competition.drivercontrol(Usercontrol);
   Competition.autonomous(Autonomous);
 
