68f4d8cd9c3145a15c268dba7a1f227661e7b431
Russel Janairo <61097770+rjanairo@users.noreply.github.com>
2021-11-19 02:37:02 -0800

    Auton with arm and simple direction

--- Zika Code Build/src/routines.cpp
+++ Zika Code Build/src/routines.cpp
@@ -2,9 +2,9 @@
 #include "autonomous-functions.h"
 
 void RoutineOne(){ //Auton Arm back included, bot needs to face backwards
-  MoveReverse(51, 80, 5);
+  MoveReverse(51, 80, 5); //bot moves from backwards assuming the arm is still in backside
   wait(300, msec);
-  IntakeControl(90, 90.5);
+  IntakeControl(90, 90.5); //arm close down to take the mobile goal
   wait(300,  msec);
-  Movefoward(50.5, 100, 3);
+  Movefoward(50.5, 100, 3); //bot returns back to the starting point
 }
