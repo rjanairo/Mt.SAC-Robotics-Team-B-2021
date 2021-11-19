395b9a9ca5dd401bf94f6a7d74a788e4aedceb36
Russel Janairo <61097770+rjanairo@users.noreply.github.com>
2021-11-19 03:25:03 -0800

    Update routines.cpp

--- Zika Code Build/src/routines.cpp
+++ Zika Code Build/src/routines.cpp
@@ -2,9 +2,9 @@
 #include "autonomous-functions.h"
 
 void RoutineOne(){ //Auton Arm back included, bot needs to face backwards
-  MoveReverse(51, 80, 5); //bot moves from backwards assuming the arm is still in backside
+  MoveReverse(125, 50, 10); //bot moves from backwards assuming the arm is still in backside
   wait(300, msec);
   IntakeControl(90, 90.5); //arm close down to take the mobile goal
   wait(300,  msec);
-  Movefoward(50.5, 100, 3); //bot returns back to the starting point
+  Movefoward(125, 50, 10); //bot returns back to the starting point
 }
