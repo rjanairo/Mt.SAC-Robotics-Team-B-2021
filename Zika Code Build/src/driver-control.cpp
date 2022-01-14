29cbc5dba324ba127b6f212e8efa43ef8bbbe159
Russel Janairo <61097770+rjanairo@users.noreply.github.com>
2022-01-13 17:58:39 -0800

    Update 1/13/2022

--- Zika Code Build/src/driver-control.cpp
+++ Zika Code Build/src/driver-control.cpp
@@ -1,89 +1,67 @@
-#include "vex.h"
-#include "driver-control.h"
-#include "robot-config.h"
+#include "autonomous-functions.h"
 
-using namespace vex;
 
-//defining functions from header files
-void DriveArcade(){
-
-  if(abs(Controller1.Axis3.value()) > 5 || abs(Controller1.Axis1.value()) > 5){
-    LeftDrive.spin(forward, ( Controller1.Axis3.value() + Controller1.Axis1.value() )/2 , pct);
-    RightDrive.spin(forward, ( Controller1.Axis3.value() - Controller1.Axis1.value() )/2 , pct);
-  } 
-  else{
-    LeftDrive.stop();
-    RightDrive.stop();
+// Set the time for the motors to perform their task until forced stop
+  void SetMotorTimeout(int TimeSeconds){
+    LeftDrive.setTimeout(TimeSeconds, sec);
+    RightDrive.setTimeout(TimeSeconds, sec);
   }
-}
 
-void DriveTank(){
-   if( abs( Controller1.Axis2.value() ) > 5 && abs( Controller1.Axis3.value() ) > 5 ){
-     LeftDrive.spin(forward,Controller1.Axis3.value(), pct);
-     RightDrive.spin(forward, Controller1.Axis2.value(),pct);
-   } 
-   else{
-     LeftDrive.stop();
-     RightDrive.stop();
-   }
+// Drive Foward with a specified distance in Centimeters
+  void Movefoward(float DistancecCM, int SpeedPct, int TimeoutSec){
+    SetMotorTimeout(TimeoutSec);
 
+    LeftDrive.rotateFor(forward, ( DistancecCM / g_wheelCircumference ) * g_driveTrainGearRatio , rev, false); 
+    RightDrive.rotateFor(forward, ( DistancecCM / g_wheelCircumference ) * g_driveTrainGearRatio , rev);
+
+    SetMotorTimeout(0);
+  }
+
+// Drive Reverse with a specified distance in Centimeters
+  void MoveReverse(float DistancecCM, int SpeedPct, int TimeoutSec){
+    SetMotorTimeout(TimeoutSec);
+
+    LeftDrive.rotateFor(reverse, ( DistancecCM / g_wheelCircumference ) * g_driveTrainGearRatio , rev, false); 
+    RightDrive.rotateFor(reverse, ( DistancecCM / g_wheelCircumference ) * g_driveTrainGearRatio , rev);
+
+    SetMotorTimeout(0);
+
+  }
+//
+
+//Rotate Robot clockwise a specified degree rotation in Degrees
+  void PointTurnClockwise(float ThetaDegree, int SpeedPct, int TimeoutSec){
+  SetMotorTimeout(TimeoutSec);
+
+  LeftDrive.rotateFor(forward, ((g_robotRadius * (ThetaDegree * (M_PI/180) ) ) / g_wheelCircumference) * g_driveTrainGearRatio  , rev, false); 
+  RightDrive.rotateFor(reverse, ((g_robotRadius * (ThetaDegree * (M_PI/180) ) ) / g_wheelCircumference) * g_driveTrainGearRatio   , rev);
+
+  SetMotorTimeout(0);
+
+  }
+//
+
+//Rotate Robot Counter clockwise a specified degree rotation in Degrees
+  void PointTurnCounterClockwise(float ThetaDegree, int SpeedPct, int TimeoutSec){
+  SetMotorTimeout(TimeoutSec);
+
+  LeftDrive.rotateFor(reverse, ((g_robotRadius * (ThetaDegree * (M_PI/180) ) ) / g_wheelCircumference) * g_driveTrainGearRatio  , rev, false); 
+  RightDrive.rotateFor(forward, ((g_robotRadius * (ThetaDegree * (M_PI/180) ) ) / g_wheelCircumference) * g_driveTrainGearRatio   , rev);
+
+  SetMotorTimeout(0);
+  }
+//
+ void intakeAuton(int intakeDegree, int intakeVelocity){
+  IntakeMotorA.spinTo(intakeDegree, rotationUnits::deg , intakeVelocity, velocityUnits::pct, true);
+  IntakeMotorB.spinTo(intakeDegree, rotationUnits::deg , intakeVelocity, velocityUnits::pct, true);
  }
 
-void IntakeControl(){
-  if(Controller1.ButtonL1.pressing()){
-    IntakeMotorA.spin(forward, 70, pct);
-    IntakeMotorB.spin(forward, 70, pct);
-  }else if(Controller1.ButtonL2.pressing()){
-    IntakeMotorA.spin(reverse, 70, pct);
-    IntakeMotorB.spin(reverse, 70, pct);
-  }
-  else{
-    IntakeMotorA.stop(hold);
-    IntakeMotorB.stop(hold);
-  }
-} 
+ void clawFangAuton(int clawFangDegree, float clawFangVelocity){
+    clawFangMotor.spinTo(clawFangDegree, rotationUnits::deg, clawFangVelocity, velocityUnits::pct, true);
+ } 
 
-void clawFangControl(){
-  if(Controller1.ButtonR1.pressing()){
-    clawFang.spin(forward);
-  }
-  else if(Controller1.ButtonR2.pressing())
-    {
-    clawFang.spin(reverse);
-  }
-  else {
-    clawFang.stop(hold);    
-  }
-}
-//void LiftControl(){
-  //if(Controller1.ButtonR1.pressing()){
-    //liftA.spin(reverse,100, pct);
-    //liftB.spin(reverse,100, pct);
-  //}else if(Controller1.ButtonR2.pressing()){
-    //liftA.spin(forward,100, pct);
-    //liftB.spin(forward,100, pct);
-  //}else{
-    //liftA.stop(hold);
-    //liftB.stop(hold);
-  //}
-//}
-
-
-void DriveXDrive(){
-
-  //Folllowing code is for a 4 wheel omni x-drive. 
-  //Assuming that when Axis 4 is held in the positive direction, all wheels produce a movement that moves robot foward. 
-
-  if(abs(Controller1.Axis1.value()) > 5 || abs(Controller1.Axis3.value()) > 5 || abs(Controller1.Axis4.value()) > 5){
-    LeftDrive.spin(forward, (Controller1.Axis4.value() + Controller1.Axis1.value() + Controller1.Axis3.value())/3 , pct);
-    LeftDrive.spin(forward, (Controller1.Axis4.value() + Controller1.Axis1.value() - Controller1.Axis3.value())/3 , pct);
-    RightDrive.spin(forward, (Controller1.Axis4.value() - Controller1.Axis1.value() - Controller1.Axis3.value())/3 , pct);
-    RightDrive.spin(forward, (Controller1.Axis4.value() - Controller1.Axis1.value() + Controller1.Axis3.value())/3 , pct);
-  } else {
-    LeftDrive.stop();
-    LeftDrive.stop();
-
-    RightDrive.stop();
-    RightDrive.stop();
-  }
+//Stop all motors
+void DriveStop(){
+  LeftDrive.stop(hold);
+  RightDrive.stop(hold);
 }
